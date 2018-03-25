6ae32d235d9679f5c68ff8fb13ad1b4585c52eab
delvedor <tommydelved@gmail.com>
2018-03-25 14:48:45 +0200

    Updated test

--- test.js
+++ test.js
@@ -9,42 +9,50 @@ var url = 'mongodb://localhost:27017/mongocleantest'
 var baseCount = !process.env.MONGODB_VERSION || semver.satisfies(process.env.MONGODB_VERSION, '>= 3.2.0') ? 0 : 1
 
 function getDB (cb) {
-  MongoClient.connect(url, { w: 1 }, cb)
+  MongoClient.connect(url, { w: 1 }, (err, client) => {
+    if (err) return cb(err, null)
+    cb(null, client, client.db('mongocleantest'))
+  })
 }
 
-function close (db, t) {
-  db.close(function () {
+function close (client, t) {
+  client.close(function () {
     t.end()
   })
 }
 
-function cleanVerifyAndClose (db, t) {
-  clean(db, function (err, db) {
+function cleanVerifyAndClose (client, db, t) {
+  clean(db, function (err, cleanDb, cleanClient) {
     t.notOk(err, 'no error')
 
-    db.listCollections({}).toArray(function (err, collections) {
+    var dbSwap = typeof db === 'string' ? cleanDb : db
+    dbSwap.listCollections({}).toArray(function (err, collections) {
       t.notOk(err, 'no error')
 
       t.equal(collections.length, baseCount + 0)
 
-      close(db, t)
+      if (typeof db === 'string') {
+        close(cleanClient, t)
+      } else {
+        close(client, t)
+      }
     })
   })
 }
 
 test('does nothing on an empty db', function (t) {
-  getDB(function (err, db) {
+  getDB(function (err, client, db) {
     t.notOk(err, 'no error')
 
     clean(db, function (err) {
       t.notOk(err, 'no error')
-      close(db, t)
+      close(client, t)
     })
   })
 })
 
 test('removes a collection', function (t) {
-  getDB(function (err, db) {
+  getDB(function (err, client, db) {
     t.notOk(err, 'no error')
 
     // creates collection dummy1
@@ -52,13 +60,13 @@ test('removes a collection', function (t) {
       t.notOk(err, 'no error')
 
       // clean, verify and close
-      cleanVerifyAndClose(db, t)
+      cleanVerifyAndClose(client, db, t)
     })
   })
 })
 
 test('removes two collections', function (t) {
-  getDB(function (err, db) {
+  getDB(function (err, client, db) {
     t.notOk(err, 'no error')
 
     db.createCollection('dummy1', function (err) {
@@ -68,29 +76,29 @@ test('removes two collections', function (t) {
         t.notOk(err, 'no error')
 
         // clean, verify and close
-        cleanVerifyAndClose(db, t)
+        cleanVerifyAndClose(client, db, t)
       })
     })
   })
 })
 
 test('clean using an url', function (t) {
-  getDB(function (err, db) {
+  getDB(function (err, client, db) {
     t.notOk(err, 'no error')
 
     // creates collection dummy1
     db.createCollection('dummy3', function (err) {
       t.notOk(err, 'no error')
 
-      db.close(function () {
-        cleanVerifyAndClose(url, t)
+      client.close(function () {
+        cleanVerifyAndClose(client, url, t)
       })
     })
   })
 })
 
 test('removes two collections on three', function (t) {
-  getDB(function (err, db) {
+  getDB(function (err, client, db) {
     t.notOk(err, 'no error')
 
     db.createCollection('dummy1', function (err) {
@@ -112,7 +120,7 @@ test('removes two collections on three', function (t) {
 
               t.equal(collections.length, baseCount + 1)
 
-              close(db, t)
+              close(client, t)
             })
           })
         })
@@ -122,7 +130,7 @@ test('removes two collections on three', function (t) {
 })
 
 test('removes two collections on four connecting via url', function (t) {
-  getDB(function (err, db) {
+  getDB(function (err, client, db) {
     t.notOk(err, 'no error')
 
     db.createCollection('dummy1', function (err) {
@@ -134,16 +142,16 @@ test('removes two collections on four connecting via url', function (t) {
         db.createCollection('dummy3', function (err) {
           t.notOk(err, 'no error')
 
-          db.close(function () {
-            clean(url, {exclude: ['dummy1', 'dummy2']}, function (err, db) {
+          client.close(function () {
+            clean(url, {exclude: ['dummy1', 'dummy2']}, function (err, cleanDb, cleanClient) {
               t.notOk(err, 'no error')
 
-              db.listCollections({}).toArray(function (err, collections) {
+              cleanDb.listCollections({}).toArray(function (err, collections) {
                 t.notOk(err, 'no error')
 
                 t.equal(collections.length, baseCount + 2)
 
-                close(db, t)
+                close(cleanClient, t)
               })
             })
           })
@@ -154,7 +162,7 @@ test('removes two collections on four connecting via url', function (t) {
 })
 
 test('removes all the content from a collection', function (t) {
-  getDB(function (err, db) {
+  getDB(function (err, client, db) {
     t.notOk(err, 'no error')
 
     clean(db, function (err, db) {
@@ -171,7 +179,7 @@ test('removes all the content from a collection', function (t) {
               coll.find().count(function (err, count) {
                 t.error(err)
                 t.equal(count, 0, 'no elements in the collection')
-                close(db, t)
+                close(client, t)
               })
             })
           })
